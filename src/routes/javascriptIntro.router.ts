e21e6d88a278ba487f9949bc08574029e84a1054
Brian Joseph <brianjosephstudio@gmail.com>
2024-05-20 20:48:03 +0200

    Fixed route to serve react app

--- src/routes/javascriptIntro.router.ts
+++ src/routes/javascriptIntro.router.ts
@@ -5,21 +5,21 @@ import { useProxyIfDev } from '../util/middleware/devProxy'
 
 const router = express.Router()
 
-const appFolder = path.resolve(__dirname, "..", "..", "..", "apps", "javascript-intro")
+const appFolder = path.resolve(__dirname, "..", "..", "apps", "javascript-intro")
 
-//This end point handles a request for the static light mode html file
+
 router.get("/light-mode", (req, res) => {
-    res.sendFile(path.resolve(appFolder, "public", "lightMode.html"))
+    res.sendFile(path.resolve(appFolder, "static", "lightMode.html"))
 })
 
-//This end point handles a request for the static dark mode html file
 router.get("/dark-mode", (req, res) => {
-    res.sendFile(path.resolve(appFolder, "public", "darkMode.html"))
+    res.sendFile(path.resolve(appFolder, "static", "darkMode.html"))
 })
 
-//This end point handles a request to the main app.
-// router.get("/", useProxyIfDev(process.env.JAVASCRIPT_INTRO_PORT), (req, res) => {
-//     res.sendFile(path.resolve(appFolder, "dist", "index.html"))
-// })
+router.use("/", express.static(path.resolve(appFolder)))
+
+router.use("*", (req, res) => {
+    res.sendFile(path.resolve(appFolder, "index.html"))
+})
 
 export default router
